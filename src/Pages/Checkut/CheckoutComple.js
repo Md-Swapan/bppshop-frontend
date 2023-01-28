bfe0e73ea1822c419cd495384bc235d5f414ba63
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-28 10:19:27 +0600

    payment-complete update

--- src/Pages/Checkut/CheckoutComple.js
+++ src/Pages/Checkut/CheckoutComple.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./CheckoutComplete.css";
 
 const CheckoutComple = () => {
@@ -6,7 +7,7 @@ const CheckoutComple = () => {
     <div>
       <div class="row d-flex justify-content-center">
         <div class="col-md-10 col-lg-10">
-          <div class="card">
+          <div class="checkout_comple_card">
             <div class=" p-5">
               <div class="row">
                 <div class="col-md-6">
@@ -16,9 +17,9 @@ const CheckoutComple = () => {
 
               <div class="row mb-4">
                 <div class="col-12">
-                  <center>
-                    <i class="fa fa-check-circle"></i>
-                  </center>
+                  <div className="my-5 text-center">
+                  <i class="bi bi-check-circle-fill fw-bold fs-1 check_fill_icon"></i>
+                  </div>
                 </div>
               </div>
 
@@ -28,20 +29,13 @@ const CheckoutComple = () => {
                 the method you selected!
               </span>
 
-              <div class="row mt-4">
-                <div class="col-6">
-                  <a href="https://bppshop.com.bd" class="btn btn-primary">
-                    Go to shopping
-                  </a>
+              <div class="d-flex justify-content-between align-items-center my-5">
+                <div className="go_to_shopping">
+                  <Link to="/">Go to shopping</Link>
                 </div>
 
-                <div class="col-6">
-                  <a
-                    href="https://bppshop.com.bd/account-oder"
-                    class="btn btn-secondary pull-right"
-                  >
-                    Check orders
-                  </a>
+                <div className="got_to_check_order">
+                  <Link to="/profile/orders">Check orders</Link>
                 </div>
               </div>
             </div>
