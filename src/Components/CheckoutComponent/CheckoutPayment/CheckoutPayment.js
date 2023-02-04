5f2b9c022caab90fd478a09de4e7f0f61f644190
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-02-04 17:37:26 +0600

    check out success

--- src/Components/CheckoutComponent/CheckoutPayment/CheckoutPayment.js
+++ src/Components/CheckoutComponent/CheckoutPayment/CheckoutPayment.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "./CheckoutPayment.css";
 import confirmImg from "../../../Assets/Images/confirm_order_img.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { baseUrl } from "../../../BaseUrl/BaseUrl";
 
 const CheckoutPayment = () => {
+  const { shippingAddressInfo } = useSelector((state) => state?.shippingInfo);
+  const token = localStorage.getItem("token");
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+
+  const navigate=useNavigate();
+  const handleCheckoutConfirm = (id) => {
+    const address_id = {
+      address_id: id,
+    };
+    axios.post(`${baseUrl}/cart/checkout`,address_id, config)
+    .then(res=>{
+      if (res.data.status==="success") {
+        navigate('/checkout-complete')
+      }
+    })
+  };
+
   return (
     <div>
       <h3 className="mb-4">Payment method</h3>
       <hr />
       <h6 class="my-5">Choose payment</h6>
       <div>
-        <Link to="/checkout-complete">
-        <div className="confirm_order_img">
+        <div onClick={()=>handleCheckoutConfirm(shippingAddressInfo?.data?.id)} className="confirm_order_img">
           <img alt="" src={confirmImg} />
         </div>
-        </Link>
       </div>
       <div className="back_to_shipping">
         <Link to="/shipping-details">
