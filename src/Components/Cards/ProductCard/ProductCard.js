ad2c055ab10cee95772466e02d684e88235af318
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-02-06 18:17:53 +0600

    product card responsive

--- src/Components/Cards/ProductCard/ProductCard.js
+++ src/Components/Cards/ProductCard/ProductCard.js
@@ -94,7 +94,7 @@ const ProductCard = ({ product }) => {
                   alt=""
                 />
                 <div className="product-card-body-content">
-                  <small>{name.toString().substring(0, 20)}...</small>
+                  <small>{name.toString().substring(0, 15)}...</small>
                   <br />
                   <div className="product-card-body-content-unit-price">
                     <span>
