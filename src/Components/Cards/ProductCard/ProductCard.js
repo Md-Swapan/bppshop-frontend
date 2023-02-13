5834265f369d109d598f6c3ab4b75feb0f83c6c2
Md-Swapan <swapan@excelitai.com>
2023-02-13 10:58:57 +0600

    responsive update and address update

--- src/Components/Cards/ProductCard/ProductCard.js
+++ src/Components/Cards/ProductCard/ProductCard.js
@@ -104,7 +104,7 @@ const ProductCard = ({ product }) => {
                   <img src={defaultProImg} alt="" />
                 )}
                 <div className="product-card-body-content">
-                  <small>{name.toString().substring(0, 20)}...</small>
+                  <small>{name.toString().substring(0, 15)}...</small>
                   <br />
                   <div className="product-card-body-content-unit-price">
                     <span>
