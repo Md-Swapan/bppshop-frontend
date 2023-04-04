267a86281cec65906540c7963d9bca93209adfa3
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-04-04 16:39:05 +0600

    shiping update

--- src/Components/ShippingComponent/AddShipping/AddShipping.js
+++ src/Components/ShippingComponent/AddShipping/AddShipping.js
@@ -49,7 +49,6 @@ const AddShipping = () => {
       .then((res) => setAreaDataOptions(res.data.data));
   };
 
-
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
     const district_id = districtId;
@@ -58,136 +57,127 @@ const AddShipping = () => {
 
     dispatch(addShippingAddress(newData));
     dispatch(getDeliveryCharge(district_id));
-
     if (shippingAddressInfo?.status === "success") {
       navigate("/shipping-details");
     }
   };
 
-  // useEffect(() => {
-  //   if (shippingAddressInfo?.data?.is_billing === "1") {
-  //     dispatch(getDeliveryCharge(shippingAddressInfo?.data?.district_id));
-  //   }
-  // }, [dispatch, shippingAddressInfo]);
-
-  // console.log(shippingAddressInfo);
-
   return (
     <>
-    <MetaData title="Add-New-Delivery-Address - BPPShop" />
+      <MetaData title="Add-New-Delivery-Address - BPPShop" />
       <div className="shipping_Add_container">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="shipping_Add_content">
-          <div className="shipping_Add_header">Add New Delivery Address</div>
-        </div>
-        <hr className="shipping_Add_line" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="shipping_Add_content">
+            <div className="shipping_Add_header">Add New Delivery Address</div>
+          </div>
+          <hr className="shipping_Add_line" />
 
-        <div className="shipping_address_input_container">
-          <div className="form-group">
-            <span>Contact person name *</span>
-            <input
-              {...register("contact_person_name", { required: true })}
-              name="contact_person_name"
-              className="shipping_address_input"
-              type="text"
-              placeholder="Enter Your Name"
-            />
-          </div>
-          <div className="form-group">
-            <span>Phone *</span>
-            <input
-              {...register("phone", { required: true })}
-              name="phone"
-              className="shipping_address_input"
-              type="text"
-              placeholder="Enter Your Phone Number"
-            />
-          </div>
-          <div className="form-group">
-            <span>District/City*</span>
-            <select
-              // {...register("district_id", { required: true })}
-              onChange={handleDistrictChange}
-              required
-              name="district_id"
-              className="shipping_address_input"
-              aria-label="Default select example"
-            >
-              <option value={null} selected>
-                ------Select District/City------
-              </option>
-              {districtDataOptions?.map((district) => (
-                <option key={district.id} value={district.id}>
-                  {district.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="form-group">
-            <span>Upazila/Thana*</span>
-            <select
-              // {...register("upazila_id", { required: true })}
-              onChange={handleThanaChange}
-              required
-              name="upazila_id"
-              className="shipping_address_input"
-              aria-label="Default select example"
-            >
-              <option value={null} selected>
-                ------Select Upazila/Thana------
-              </option>
-              {thanaDataOptions.map((thana) => (
-                <option key={thana.id} value={thana.id}>
-                  {thana.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="form-group">
-            <span>Area*</span>
-            <select
-              {...register("area_id", { required: true })}
-              name="area_id"
-              className=" shipping_address_input"
-              aria-label="Default select example"
-            >
-              <option selected>------Select Area------</option>
-              {areaDataOptions.map((area) => (
-                <option key={area.id} value={area.id}>
-                  {area.name}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="form-group">
-            <span>Address *</span>
-            <input
-              {...register("address", { required: true })}
-              name="address"
-              className="shipping_address_input"
-              type="text"
-              placeholder="House no. / Building /Street /Area"
-            />
-          </div>
-        </div>
-
-        <div className="shipping_add_close_btn">
-          <div>
-            <Link to="/shipping-details">
+          <div className="shipping_address_input_container">
+            <div className="form-group">
+              <span>Contact person name *</span>
               <input
-                className="shipping_close_btn"
-                type="button"
-                value="close"
+                {...register("contact_person_name", { required: true })}
+                name="contact_person_name"
+                className="shipping_address_input"
+                type="text"
+                placeholder="Enter Your Name"
               />
-            </Link>
+            </div>
+            <div className="form-group">
+              <span>Phone *</span>
+              <input
+                {...register("phone", { required: true })}
+                name="phone"
+                className="shipping_address_input"
+                type="text"
+                placeholder="Enter Your Phone Number"
+              />
+            </div>
+            <div className="form-group">
+              <span>District/City*</span>
+              <select
+                // {...register("district_id", { required: true })}
+                onChange={handleDistrictChange}
+                required
+                name="district_id"
+                className="shipping_address_input"
+                aria-label="Default select example"
+              >
+                <option value={null} selected>
+                  ------Select District/City------
+                </option>
+                {districtDataOptions?.map((district) => (
+                  <option key={district.id} value={district.id}>
+                    {district.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <span>Upazila/Thana*</span>
+              <select
+                // {...register("upazila_id", { required: true })}
+                onChange={handleThanaChange}
+                required
+                name="upazila_id"
+                className="shipping_address_input"
+                aria-label="Default select example"
+              >
+                <option value={null} selected>
+                  ------Select Upazila/Thana------
+                </option>
+                {thanaDataOptions.map((thana) => (
+                  <option key={thana.id} value={thana.id}>
+                    {thana.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <span>Area*</span>
+              <select
+                {...register("area_id", { required: true })}
+                name="area_id"
+                className=" shipping_address_input"
+                aria-label="Default select example"
+              >
+                <option selected>------Select Area------</option>
+                {areaDataOptions.map((area) => (
+                  <option key={area.id} value={area.id}>
+                    {area.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="form-group">
+              <span>Address *</span>
+              <input
+                {...register("address", { required: true })}
+                name="address"
+                className="shipping_address_input"
+                type="text"
+                placeholder="House no. / Building /Street /Area"
+              />
+            </div>
           </div>
-          <div>
-            <input className="shipping_save_btn" type="submit" value="save" />
+
+          <div className="shipping_add_close_btn">
+            <div>
+              <Link to="/shipping-details">
+                <input
+                  className="shipping_close_btn"
+                  type="button"
+                  value="close"
+                />
+              </Link>
+            </div>
+            <div>
+              <input className="shipping_save_btn" type="submit" value="save" />
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     </>
   );
 };
--- src/Components/ShippingComponent/ShippingAddressList/ShippingAddressList.js
+++ src/Components/ShippingComponent/ShippingAddressList/ShippingAddressList.js
@@ -4,9 +4,12 @@ import "./ShippingAddressList.css";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { loadAllShippingAddress } from "../../../Redux/Actions/ShippingAddressAction";
-import { setDefaultShippingAddress } from './../../../Redux/Actions/ShippingAddressAction';
-import { getDeliveryCharge } from '../../../Redux/Actions/DeliveryChargeAction';
+import { setDefaultShippingAddress } from "./../../../Redux/Actions/ShippingAddressAction";
+import { getDeliveryCharge } from "../../../Redux/Actions/DeliveryChargeAction";
 import MetaData from "../../../Pages/Layout/MetaData";
+import axios from "axios";
+import { baseUrl } from "../../../BaseUrl/BaseUrl";
+import { toast } from "react-hot-toast";
 
 const ShippingAddressList = () => {
   const dispatch = useDispatch();
@@ -16,89 +19,132 @@ const ShippingAddressList = () => {
     dispatch(loadAllShippingAddress());
   }, [dispatch]);
 
-  const { allShippingAddressInfo } = useSelector((state) => state.allShippingInfo);
-  const {shippingAddressInfo} = useSelector((state) => state.shippingInfo);
+  const { allShippingAddressInfo } = useSelector(
+    (state) => state.allShippingInfo
+  );
+  // console.log(allShippingAddressInfo?.data);
+  const { shippingAddressInfo } = useSelector((state) => state.shippingInfo);
 
   const handleSetDefaultAddress = (address_id) => {
     const addressId = {
-      address_id : address_id
-    }
+      address_id: address_id,
+    };
 
-    dispatch(setDefaultShippingAddress(addressId))
+    dispatch(setDefaultShippingAddress(addressId));
 
-    if(shippingAddressInfo?.status === "success"){
+    if (shippingAddressInfo?.status === "success") {
       navigate("/shipping-details");
     }
   };
 
+  //address detele functionality
+  const handleDeleteAddress = (shippingId, customerId) => {
+    const token = localStorage.getItem("token");
+    const deleteData = {
+      customer_id: customerId,
+      id: shippingId,
+    };
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    axios
+      .post(`${baseUrl}/shipping-address/delete-address`, deleteData, config)
+      .then((res) => {
+        // console.log(res);
+        if (res.status === 200) {
+          dispatch(loadAllShippingAddress());
+          toast.success(res.data.message, {
+            duration: 5000,
+      
+            style: {
+              width: "100%",
+              height: "80px",
+              padding: "0px 20px",
+              background: "#86bc19",
+              color: "#fff",
+            },
+          });
+        }
+      });
+  };
+
   return (
     <>
-     <MetaData title="Choose-Default-Delivery-Address - BPPShop" />
+      <MetaData title="Choose-Default-Delivery-Address - BPPShop" />
       <div>
-      <div className="shipping_container">
-        <div className="shipping_content">
-          <div className="shipping_header">Delivery Address</div>
-        </div>
-        <hr className="shipping_line" />
-        <div className="address_content">
-          <div className="row">
-            { allShippingAddressInfo && allShippingAddressInfo?.data?.map((shippingAddInfo) => (
-              <div key={shippingAddInfo?.id} className="col-md-6">
-                <div className="shipping_address_box">
-                  <div className="shipped_person">
-                    <div>
-                      Delivery to :{" "}
-                      {shippingAddInfo?.contact_person_name}
-                    </div>
-                    <div>
-                      {shippingAddInfo?.is_billing === 1 ? (
-                        <div className="default_btn">
-                          <i className="bi bi-check-circle-fill"></i>
-                        </div>
-                      ) : (
-                        <div
-                          onClick={() =>
-                            handleSetDefaultAddress(shippingAddInfo?.id)
-                          }
-                          className="set_default_btn"
-                        >
-                        <span onClick={()=> dispatch(getDeliveryCharge(shippingAddInfo?.district_id))}><i className="bi bi-check"></i> Choose</span>
-                          
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div className="shiped_address">
-                    <span className="home_text">home </span> { shippingAddInfo?.phone}{" "}
-                    | {shippingAddInfo?.data?.address}{" "}
-                    <span className="change_text">
-                      <i className="bi bi-pencil-fill"></i> Edit
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
+        <div className="shipping_container">
+          <div className="shipping_content">
+            <div className="shipping_header">Delivery Address</div>
           </div>
-          {/* <div className="add_more_address_btn">
+          <hr className="shipping_line" />
+          <div className="address_content">
+            <div className="row">
+              {allShippingAddressInfo &&
+                allShippingAddressInfo?.data?.map((shippingAddInfo) => (
+                  <div key={shippingAddInfo?.id} className="col-md-6">
+                    <div className="shipping_address_box">
+                      <div className="shipped_person">
+                        <div>
+                          Delivery to : {shippingAddInfo?.contact_person_name}
+                        </div>
+                        <div>
+                          {shippingAddInfo?.is_billing === 1 ? (
+                            <div className="default_btn">
+                              <i className="bi bi-check-circle-fill"></i>
+                            </div>
+                          ) : (
+                            <div
+                              onClick={() =>
+                                handleSetDefaultAddress(shippingAddInfo?.id)
+                              }
+                              className="set_default_btn"
+                            >
+                              <span
+                                onClick={() =>
+                                  dispatch(
+                                    getDeliveryCharge(
+                                      shippingAddInfo?.district_id
+                                    )
+                                  )
+                                }
+                              >
+                                <i className="bi bi-check"></i> Choose
+                              </span>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="shiped_address">
+                        <span className="home_text">home </span>{" "}
+                        {shippingAddInfo?.phone} | {shippingAddInfo?.address}{" "}
+                        <span className="change_text m-2">
+                          <i className="bi bi-pencil-fill"></i> Edit
+                        </span>
+                        <span
+                          onClick={() =>
+                            handleDeleteAddress(
+                              shippingAddInfo.id,
+                              shippingAddInfo.customer_id
+                            )
+                          }
+                          className="change_text"
+                        >
+                          <i className="bi bi-trash3-fill"></i> Delete
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+
             <Link to="/add-shipping-address">
-              <div className="d-flex justify-content-center align-items-center">
-                <div>
-                  <i className="bi bi-plus"></i>
-                </div>
-                <div>Add Delivery Address </div>
-              </div>
+              <button className="add_more_address_btn">
+                <i className="bi bi-plus"></i> Add Delivery Address
+              </button>
             </Link>
-          </div> */}
-
-
-          <Link to="/add-shipping-address">
-            <button className="add_more_address_btn">
-              <i className="bi bi-plus"></i> Add Delivery Address
-            </button>
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
