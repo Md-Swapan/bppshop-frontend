023fbaf9136a35a025c3417748f06b668f7bb5a7
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-04-16 14:08:36 +0600

    review update

--- src/Pages/Review/AddProductReview.js
+++ src/Pages/Review/AddProductReview.js
@@ -1,42 +1,142 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 import "./AddProductReview.css";
-import { useSelector } from "react-redux";
+import { Rating } from "react-simple-star-rating";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { baseUrl } from "../../BaseUrl/BaseUrl";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const AddProductReview = () => {
+  const token = localStorage.getItem("token");
   const { pid } = useParams();
-const { userOrderDetails } = useSelector((state) => state?.userOrderDetails);
-// console.log(pid);
-// console.log(userOrderDetails);
-const reviewProductDetails = userOrderDetails.find((i) => i?.product_id === parseInt(pid));
-// console.log(reviewProductDetails?.product_details?.name);
+  const [productDetail, setProductDetail] = useState([]);
 
+  useEffect(() => {
+    axios.get(`${baseUrl}/products/details/${pid}`).then((res) => {
+      setProductDetail(res?.data?.data);
+    });
+  }, [pid]);
+
+  const getProductsReviewDetails = () => {
+    axios.get(`${baseUrl}/products/details/${pid}`).then((res) => {
+      setProductDetail(res?.data?.data);
+    });
+  };
+  const reversedReviews = productDetail?.reviews?.map((_, index, arr) => arr[arr.length - 1 - index]);
+
+  //rating functionality
+  const { register, handleSubmit, reset } = useForm();
+  const [rating, setRating] = useState(0);
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+  // Optinal callback functions
+  const onSubmit = (data) => {
+    const newData = {
+      product_id: pid,
+      comment: data.comment,
+      rating: rating,
+    };
+    axios
+      .post(baseUrl + `/products/reviews/submit`, newData, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        if (res?.data?.status === "success") {
+          reset();
+          setRating(rating);
+          getProductsReviewDetails();
+          // toaster
+          toast.success(res?.data?.message, {
+            duration: 5000,
+            style: {
+              width: "100%",
+              height: "80px",
+              padding: "0px 20px",
+              background: "#86bc19",
+              color: "#fff",
+            },
+          });
+          
+        } else {
+          document.getElementById("errorMsg").innerText = res?.data?.message;
+        }
+      });
+  };
 
   return (
     <div>
       <div className="comments-container">
-        <h2>Leave a Comment</h2>
-        <p>{reviewProductDetails?.product_details?.name}</p>
- 
-        <form action="">
-          <div className="input-container">
-            {/* <div className="input-container-content">
-              <label htmlFor="">Name</label>
-              <br />
-              <input type="text" />
-            </div>
-            <div className="input-container-content">
-              <label htmlFor="">Email</label>
-              <br />
-              <input type="email" />
-            </div> */}
-          </div>
-          <br />
-          <textarea name="" id="textarea" cols="30" rows="3"></textarea>
-          
+        <h2>Rating and Comment</h2>
+        <b>{productDetail.name}</b>
+        <div
+          style={{
+            direction: "ltr",
+            fontFamily: "sans-serif",
+            touchAction: "none",
+          }}
+        >
+          <Rating onClick={handleRating} allowFraction transition />
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <textarea
+            {...register("comment", { required: true })}
+            name="comment"
+            id="textarea"
+            cols="30"
+            rows="3"
+          ></textarea>
 
-          <button type="submit">Post Comment</button>
+          <div>
+            <i id="errorMsg" className="text-danger"></i>
+          </div>
+
+          <input
+            className="border-0 px-4 py-2 rounded-2 text-light bg-warning"
+            type="submit"
+            value="Post Comment"
+          />
+          {/* <button type="button" onClick={() => reset()}>Reset</button> */}
         </form>
+        <div>
+          {reversedReviews &&
+            reversedReviews.map((review) => (
+              <div key={review.id} className="review-cart">
+                <div className="user-img">
+                  <img src="/img/userimg (1).webp" alt="" />
+                </div>
+                <div className="users-content">
+                  <div className="user-qa-header">
+                    <p>
+                      <i className="bi bi-person"></i> {review?.customer?.name}
+                    </p>
+                    <p className="mx-2">
+                      <i className="bi bi-clock"></i>{" "}
+                      {review?.created_at.slice(0, 10)}
+                    </p>
+                    <p className="rating-star">
+                      {(() => {
+                        let userRating = [];
+                        for (let i = 1; i <= review.rating; i++) {
+                          userRating.push(<i className="bi bi-star-fill"></i>);
+                        }
+                        let remaining = 5 - review.rating;
+                        for (let i = 1; i <= remaining; i++) {
+                          userRating.push(
+                            <i className="bi bi-star-fill last"></i>
+                          );
+                        }
+                        return userRating;
+                      })()}
+                    </p>
+                  </div>
+                  <small>{review?.comment}</small>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
