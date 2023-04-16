29d1282ec1187211e0b27223f88252a566c42a1e
Md-Swapan <swapan@excelitai.com>
2023-04-16 15:05:46 +0600

    update

--- src/Pages/Review/AddProductReview.js
+++ src/Pages/Review/AddProductReview.js
@@ -2,22 +2,40 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "./AddProductReview.css";
 import { useSelector } from "react-redux";
+import { Rating } from "react-simple-star-rating";
 
 const AddProductReview = () => {
   const { pid } = useParams();
-const { userOrderDetails } = useSelector((state) => state?.userOrderDetails);
-// console.log(pid);
-// console.log(userOrderDetails);
-const reviewProductDetails = userOrderDetails.find((i) => i?.product_id === parseInt(pid));
-// console.log(reviewProductDetails?.product_details?.name);
-
+  const { userOrderDetails } = useSelector((state) => state?.userOrderDetails);
+  // console.log(pid);
+  // console.log(userOrderDetails);
+  const reviewProductDetails = userOrderDetails.find(
+    (i) => i?.product_id === parseInt(pid)
+  );
+  // console.log(reviewProductDetails?.product_details?.name);
 
   return (
     <div>
+      
+
+
+
+      
       <div className="comments-container">
         <h2>Leave a Comment</h2>
         <p>{reviewProductDetails?.product_details?.name}</p>
- 
+        <div
+        style={{
+          direction: "ltr",
+          fontFamily: "sans-serif",
+          touchAction: "none",
+          
+        }}
+      >
+        <Rating allowFraction onClick={function noRefCheck() {}} transition size= {24} />
+      </div>
+
+      
         <form action="">
           <div className="input-container">
             {/* <div className="input-container-content">
@@ -33,7 +51,6 @@ const reviewProductDetails = userOrderDetails.find((i) => i?.product_id === pars
           </div>
           <br />
           <textarea name="" id="textarea" cols="30" rows="3"></textarea>
-          
 
           <button type="submit">Post Comment</button>
         </form>
