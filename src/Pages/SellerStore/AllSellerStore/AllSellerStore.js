7c883eaeebe2931d9f4679b26581d358e76d76be
Md-Swapan <swapan@excelitai.com>
2023-07-27 17:50:32 +0600

    seller updating

--- src/Pages/SellerStore/AllSellerStore/AllSellerStore.js
+++ src/Pages/SellerStore/AllSellerStore/AllSellerStore.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { baseUrl } from "../../../BaseUrl/BaseUrl";
 import { useRef } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "../SellerStore.css";
 
 const AllSellerStore = () => {
   const [allSellerStore, setAllSellerStore] = useState([]);
@@ -12,23 +14,19 @@ const AllSellerStore = () => {
   const [lastList, setLastList] = useState(false);
 
   useEffect(() => {
-    axios.get(`${baseUrl}/seller/all`).then((res) => console.log(res));
-  });
-
-  useEffect(() => {
-    let limit = 50;
+    let limit = 20;
     const fetchData = async () => {
       const response = await axios.get(
         `${baseUrl}/seller/all?limit=${limit}&offset=${currPage}`
       );
-      // console.log(response.data.data.data);
+      console.log(response.data.data);
       response && setLoading(false);
-      if (!response?.data?.data?.data?.length) {
+      if (!response?.data?.data?.length) {
         setLastList(true);
         return;
       }
       setPrevPage(currPage);
-      setAllSellerStore([...allSellerStore, ...response?.data?.data?.data]);
+      setAllSellerStore([...allSellerStore, ...response?.data?.data]);
     };
     if (!lastList && prevPage !== currPage) {
       fetchData();
@@ -45,17 +43,63 @@ const AllSellerStore = () => {
   };
   return (
     <>
-      <div>
-        <div
-          onScroll={onScroll}
-          ref={listInnerRef}
-          style={{ height: "100vh", overflowY: "auto" }}
-          className="brand_container mt-4 pb-5"
-        >
-          All Seller
+      <h4>All Seller Store:</h4>
+      <div
+        onScroll={onScroll}
+        ref={listInnerRef}
+        style={{ height: "80vh", overflowY: "auto" }}
+        className="all-seller-store-container mt-4 pb-5"
+      >
+        <SkeletonTheme baseColor="#DDDDDD" highlightColor="#e3e3e3">
+          {loading ? (
+            <>
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+              <Skeleton height="250px" borderRadius="10px" count={1} />
+            </>
+          ) : (
+            allSellerStore &&
+            allSellerStore.map((sellerStore) => (
+              // <Link key={brand?.id} to={`/brands/${brand?.name}/${brand?.id}`}>
+              <div className="seller-store-content">
+                <div className="seller-store-banner">
+                  <img
+                    src={`https://backend.bppshop.com.bd/storage/shop/banner/${sellerStore?.banner}`}
+                    alt=""
+                  />
+                </div>
 
-
-        </div>
+                <div className="seller-store-profile-container">
+                  <div className="">
+                  <div className="seller-profile-image">
+                    <img
+                      src={`https://backend.bppshop.com.bd/storage/shop/${sellerStore?.image}`}
+                      alt=""
+                    />
+                  </div>
+                    <p>{sellerStore?.name}</p>
+                  </div>
+                </div>
+              </div>
+              // </Link>
+            ))
+          )}
+        </SkeletonTheme>
       </div>
     </>
   );
